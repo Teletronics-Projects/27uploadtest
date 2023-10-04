8093615b374a0f76a683d4abc3931a26511cc6b8
Itumeleng Tele <itumeleng.tele@gmail.com>
2023-10-04 19:19:10 +0200

    storage changed to blob

--- app.js
+++ app.js
@@ -1,63 +1,88 @@
 const express = require('express');
-const multer = require('multer');
-const mysql = require('mysql2/promise');
+const expressFileUpload = require('express-fileupload');
+const { BlobServiceClient, StorageSharedKeyCredential } = require('@azure/storage-blob');
+
+// Create an instance of the Express application
 const app = express();
+const port = process.env.PORT || 3000;
 
-// MySQL database connection configuration
-const db = mysql.createPool({
-  host: 'apphostdbserver.mysql.database.azure.com',
-  user: 'AzureAdmin',
-  password: 'Itumeleng@1996',
-  database: 'apphostdb',
-});
+// Middleware for parsing JSON and form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// Set up Multer for file uploads
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/');
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  },
-});
+// Middleware for file uploads
+app.use(expressFileUpload());
 
-const upload = multer({ storage: storage });
+// Azure Blob Storage configuration
+const storageAccountName = 'apphoststorageacc1021';
+const storageAccountKey = 'OD9gz8wA0nn5HGxK/Sb71R7lHQ88OklEsj3YMNMeVKkLQ3FCl44nftUgXqVowoLWZHboOia+0VQ1+AStGc0SNA=='; // Or use Managed Identity for authentication
+const containerName = 'apphostfiles1021';
 
-// Serve static files from the 'uploads' directory
-app.use(express.static('uploads'));
+const sharedKeyCredential = new StorageSharedKeyCredential(storageAccountName, storageAccountKey);
+const blobServiceClient = new BlobServiceClient(`https://${storageAccountName}.blob.core.windows.net`, sharedKeyCredential);
+const containerClient = blobServiceClient.getContainerClient(containerName);
 
-// Define a route for uploading files
-app.post('/upload', upload.single('file'), async (req, res) => {
+// Serve uploaded files
+app.use('/files', express.static('uploads'));
+
+// Upload route
+app.post('/upload', async (req, res) => {
   try {
-    const connection = await db.getConnection();
-    const [rows] = await connection.execute('INSERT INTO files (filename) VALUES (?)', [req.file.originalname]);
-    connection.release();
-    res.status(200).send('File uploaded successfully.');
-  } catch (err) {
-    console.error(err);
+    if (!req.files || Object.keys(req.files).length === 0) {
+      return res.status(400).send('No files were uploaded.');
+    }
+
+    const uploadedFile = req.files.uploadedFile;
+    const blobName = uploadedFile.name;
+
+    const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+    const stream = uploadedFile.data;
+
+    await blockBlobClient.uploadStream(stream, undefined, undefined, {
+      blobHTTPHeaders: {
+        blobContentType: uploadedFile.mimetype
+      }
+    });
+
+    res.send('File uploaded successfully.');
+  } catch (error) {
+    console.error(error);
     res.status(500).send('Error uploading file.');
   }
 });
 
+// Download route
+app.get('/download/:fileName', async (req, res) => {
+  try {
+    const fileName = req.params.fileName;
+    const blockBlobClient = containerClient.getBlockBlobClient(fileName);
+
+    const downloadResponse = await blockBlobClient.download();
+
+    res.setHeader('Content-Type', downloadResponse.contentType);
+    res.setHeader('Content-Disposition', `attachment; filename="${fileName}"`);
+
+    downloadResponse.readableStreamBody.pipe(res);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error downloading file.');
+  }
+});
+
+// Serve the index.html file
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-// Define a route to retrieve files from the database
-app.get('/files', async (req, res) => {
-  try {
-    const connection = await db.getConnection();
-    const [rows] = await connection.execute('SELECT * FROM files');
-    connection.release();
-    res.json(rows);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Error retrieving files.');
-  }
-});
-
-// Start the server
-const port = process.env.PORT || 3000;
+// Start the Express server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+
+
+
+
+// ... (rest of the code)
+
+
